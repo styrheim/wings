7bdea1bdaf0fd8bb62911305bea8c435fa9dc983
styrheim <styrheim@olivant.fo>
2021-03-03 18:24:34 +0000

    One bird only, as the variable theBird

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,10 @@ import './App.css';
 import {Bird} from "./BIrd";
 
 function App() {
+      let theBird = <Bird wingScale={+1.00}/>
   return (
     <div className="App">
-        <Bird wingScale={+1.00}/>
-
-        <Bird wingScale={+0.90}/>
-        <Bird wingScale={+0.80}/>
-        <Bird wingScale={+0.70}/>
-        <Bird wingScale={+0.60}/>
-        <Bird wingScale={+0.50}/>
-        <Bird wingScale={+0.40}/>
-        <Bird wingScale={+0.30}/>
-        <Bird wingScale={+0.20}/>
-        <Bird wingScale={+0.10}/>
-        <Bird wingScale={+0.00}/>
-        <Bird wingScale={-0.05}/>
-        <Bird wingScale={-0.15}/>
-        <Bird wingScale={-1.00}/>
+          {theBird}
     </div>
   );
 }
