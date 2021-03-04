bb54dfcb0acbf2063aad67610569cd2a1c626b34
styrheim <styrheim@olivant.fo>
2021-03-04 10:58:44 +0000

    App changes from function to class

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import './App.css';
-import {Bird} from "./Bird";
+import {Bird, BirdProps, BirdState} from "./Bird";
 
 let theBird = <Bird wingScale={+1.00}/>
 
+interface AppProps{}
 interface AppState{
     flightTime: number; //ms since take-off
 }
 
+class App  extends React.Component<AppProps, AppState>{
 
+    render() {
+        this.run();
+        return(
+        <div className="App">
+            <br/>
+            <div>
+              {theBird}
+            </div>
+        </div>);
+    }
 
-function App() {
-    run();
-    return(
-    <div className="App">
-        <br/>
-        <div>
-          {theBird}
-        </div>
-    </div>);
-}
-
-function run(){
-    // TODO repeat
+    run(){
+        // TODO repeat
+    }
 }
 
 export default App;
