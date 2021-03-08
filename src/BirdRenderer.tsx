38e5cac7d91439d10df5580164c5562bd725fb30
styrheim <styrheim@olivant.fo>
2021-03-08 11:51:38 +0000

    Empty Collider compiled

--- src/BirdRenderer.tsx
+++ src/BirdRenderer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 export interface BirdRendererProps {
     position:  number,
     wingScale: number,
---
+++ src/Collider.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export interface ColliderProps {
+    top: number,
+    left: number,
+    height: number,
+    width: number
+}
+export class Collider extends React.Component<ColliderProps> {
+
+}
